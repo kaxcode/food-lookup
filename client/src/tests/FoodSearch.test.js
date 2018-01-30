a261d99f3a560334a206b2d8a647648c4987585b
Kenia Valladarez <kenia.valladarez@cloudreach.com>
2018-01-30 11:36:16 -0500

    Refactoring and adding assertion for th state of searchValue.

--- client/src/tests/FoodSearch.test.js
+++ client/src/tests/FoodSearch.test.js
@@ -7,19 +7,33 @@ import FoodSearch from '../FoodSearch';
 describe('FoodSearch', () => {
   let wrapper;
 
+  beforeEach(() => {
+    wrapper = shallow(<FoodSearch />);
+  });
+
+  it('should not display the remove icon', () => {
+    expect(wrapper.find('.remove.icon').length)
+      .toBe(0);
+  });
+
+  it('should display zero rows', () => {
+    expect(wrapper.find('tbody tr').length)
+      .toEqual(0);
+  });
+
   describe('user populates search field', () => {
+    const value = 'brocc';
+
     beforeEach(() => {
-      wrapper = shallow(<FoodSearch />);
+      const input = wrapper.find('input').first();
+      input.simulate('change', {
+        target: { value: value },
+      });
     });
 
-    it('should not display the remove icon', () => {
-      expect(wrapper.find('.remove.icon').length)
-        .toBe(0);
-    });
-
-    it('should display zero rows', () => {
-      expect(wrapper.find('tbody tr').length)
-        .toEqual(0);
+    it('should update state property `searchValue`', () => {
+      expect(wrapper.state().searchValue)
+        .toEqual(value);
     });
 
     describe('and API returns results', () => {
